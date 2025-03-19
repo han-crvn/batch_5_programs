be0306c0a6233bf6fd9b66e7a44a05ae0f444464
Shane <palomoshanecraven@gmail.com>
2025-03-19 20:54:34 +0800

    Fixed typo.

--- pascal_casing_name.py
+++ pascal_casing_name.py
@@ -1,8 +1,8 @@
-# Allow users to input
+# Allow users to input 
 full_name = input("Enter your full name: ")
 
 # Make it pascal casing
-pascal_case = full_name.title().replace(" ", "_")
+pascal_case = full_name.title().replace(" ", "")
 
 # Print result
 print(pascal_case)
