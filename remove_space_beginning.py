6429636fa7909fdda4c2c5e8c226bd1833f47e2b
Shane <palomoshanecraven@gmail.com>
2025-03-19 20:02:16 +0800

    Add comments and make sure the result has no leading spaces.

--- remove_space_beginning.py
+++ remove_space_beginning.py
@@ -1,1 +1,5 @@
-full_name = input("Enter your full name: ")
+# Input the full name and remove leading space
+full_name = input("Enter your full name: ").lstrip()
+
+# Print the result
+print(full_name)
