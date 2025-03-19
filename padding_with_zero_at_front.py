7adc7fddd85542cdbd46a33299e3807a6981bc27
Shane <palomoshanecraven@gmail.com>
2025-03-19 20:13:22 +0800

    Check if number is 0-1000 and add 0 padding at front.

--- padding_with_zero_at_front.py
+++ padding_with_zero_at_front.py
@@ -2,5 +2,10 @@
 numbers = (input("Enter the number: "))
 
 # Check number if 0-1000
+if 0 < int(numbers) <= 1000:
+    
+    # Add zeros to the front
+    print(numbers.zfill(6))
 
-# Add zeros to the front
+else:
+    print("Number must be 0-1000.")
